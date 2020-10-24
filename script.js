608404734cd94183a1b376a4bf768294b1c5a8ce
Shawn McPhee <scmcphee9@gmail.com>
2020-10-24 14:37:41 -0600

    added comments and cleaned the look of the code

--- script.js
+++ script.js
@@ -12,27 +12,23 @@ $("#currentDay").text(
 );
 
 // using local storage to save appointment information
-
+// when any save button is clicked, it will save that value entered in the description into the variable appointment
 $(".saveBtn").on("click", function () {
   var appointment = $(this).siblings(".description").val();
 
   console.log(appointment);
 
+  // this sets a variable for the key by selecting the id of the parent element, when the save button is clicked
   var time = $(this).parent().attr("id");
 
+  // this sets the local storage to the key of variable time and value of appointment
   localStorage.setItem(time, appointment);
   console.log(time);
 });
 
-// var savedAppointment = localStorage.getItem("hour_9");
-
-// $("#1").append(savedAppointment);
 var timeOfDay = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 for (var i = 0; i < timeOfDay.length; i++) {
-  // var firstTime = 0;
-  // var secondTime = 1;
-
   $(`#hour_${timeOfDay[i]} .description`).val(
     localStorage.getItem(`hour_${timeOfDay[i]}`)
   );
@@ -45,15 +41,15 @@ for (var i = 0; i < timeOfDay.length; i++) {
   // console.log(typeof realTime);
 
   // If statements read actual time and compare to time blocks
-  // if in the past text box turns gray
   // if the present time, turn text box red
-  // if in the future, turn text box green
   if (realTime === timeOfDay[i]) {
     $(`#textTime_${timeOfDay[i]}`).addClass("present");
   }
+  // if in the future, turn text box green
   if (realTime < timeOfDay[i]) {
     $(`#textTime_${timeOfDay[i]}`).addClass("future");
   }
+  // if in the past text box turns gray
   if (realTime > timeOfDay[i]) {
     $(`#textTime_${timeOfDay[i]}`).addClass("past");
   }
